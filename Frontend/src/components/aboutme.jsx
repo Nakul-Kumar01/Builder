5d00fe56e0d9142bf53c1af00ca6d9a8f6d710fc
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-03-14 15:50:56 +0530

    fix frontend axiosclient

--- Frontend/src/components/aboutme.jsx
+++ Frontend/src/components/aboutme.jsx
@@ -4,7 +4,7 @@ export default function Me(){
     return(
         <div className="w-full h-[80vh] bg-[#2A2825] flex justify-center items-center ">
             <div className="w-[80%] h-auto">
-                <a href="https://nakulkumar.onrender.com/" target="_blank"><img className="rounded-3xl hover:cursor-pointer" src="/me.png"></img></a>
+                <a href="https://nakulkumar.vercel.app/" target="_blank"><img className="rounded-3xl hover:cursor-pointer" src="/me.png"></img></a>
             </div>
           
         </div>
--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // configuring the axios 
 const axiosClient =  axios.create({
-    baseURL: 'https://webbuilder-0f19.onrender.com/',  //base url of backend
+    baseURL: 'https://builder126.onrender.com/',  //base url of backend
     withCredentials: true,  // it means attach cookies with request
     headers: {
         'Content-Type': 'application/json'  // data ka format json hai
